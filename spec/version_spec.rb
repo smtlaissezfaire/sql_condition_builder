56910d46d68d3ca4f498f78684abbbb1ed842c07
Scott Taylor <scott@railsnewbie.com>
2010-04-20 16:35:07 -0400

    Remove version spec

--- spec/version_spec.rb
+++
@@ -1,7 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
-
-describe SqlConditionBuilder::VERSION do
-  it "should be @ version 0.1.0" do
-    SqlConditionBuilder::VERSION.should == "0.1.0"
-  end
-end
